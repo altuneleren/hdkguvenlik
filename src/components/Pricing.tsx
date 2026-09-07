0556d690302ba38d17f1098bae6a14b1b79138e3
xFlexP_ <68639267+altuneleren@users.noreply.github.com>
2026-09-07 23:58:08 +0300

    Update Pricing.tsx

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -72,7 +72,7 @@ export default function Pricing() {
         "En çok satan paketimiz: 4 farklı noktayı sesli ve renkli gece görüşüyle kesintisiz koruma altına alın.",
       price: "15.000",
       badge: "En Çok Tercih Edilen",
-      popular: true,
+      popular: false,
       whatsappMsg: encodeURIComponent(
         "Merhaba HDK Güvenlik, 15.000 TL tutarındaki 4 Kameralı AHD Renkli ve Sesli Güvenlik Seti hakkında detaylı bilgi ve sipariş vermek istiyorum."
       ),
@@ -113,7 +113,7 @@ export default function Pricing() {
         "Geniş alanlarda kör nokta bırakmayan, 8 sesli kamera ve yüksek kapasiteli kayıt diskiyle tam kapsamlı çözüm.",
       price: "28.000",
       badge: "Geniş Alan Koruması",
-      popular: false,
+      popular: true,
       whatsappMsg: encodeURIComponent(
         "Merhaba HDK Güvenlik, 28.000 TL tutarındaki 8 Kameralı AHD Renkli ve Sesli Güvenlik Seti hakkında detaylı bilgi ve sipariş vermek istiyorum."
       ),
